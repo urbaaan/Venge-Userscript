4fe9f958b825ec2ae60d90364e52d055cdceef82
urbaaan <71067316+urbaaan@users.noreply.github.com>
2021-05-14 20:26:10 +0800

    Update graphics.js

--- graphics/graphics.js
+++ graphics/graphics.js
@@ -1,4 +1,4 @@
-/* global pc, BloomEffect, SepiaEffect, HueSaturationEffect, EdgeDetectEffect, BrightnessContrastEffect*/
+/* global pc, BloomEffect, SepiaEffect, HueSaturationEffect, EdgeDetectEffect, BrightnessContrastEffect, FxaaEffect*/
 
 const settings = {
 		'BLOOM_INTENSITY': {
@@ -26,22 +26,28 @@ const settings = {
 			text: 'Dark Effect'
 		},
 		'CONTRAST': {
+			defaultValue: 20,
+			type: 'slider',
+			text: 'Color Contrast',
+			max: 100,
+			min: 0
+		},
+		'FXAA': {
 			defaultValue: true,
 			type: 'checkbox',
-			text: 'Color Contrast'
+			text: 'Enable FXAA'
 		}
 
 	},
-	version = '1.0.2'
+	version = '1.0.1'
 
-fetch('https://raw.githubusercontent.com/urbaaan/uClient/main/mods/version.json')
+fetch('https://raw.githubusercontent.com/urbaaan/Venge-Userscript/main/graphics/version.json')
 	.then((json) => json.json())
 	.then((data) => {
 		if (data.version !== version) {
-			confirm('Latest Version is Out! Would you like to Download?')
-				.then(() => {
-					window.open()
-				})	
+			if (window.confirm('Latest Version is Out! Would you like to Download?')) {
+				window.open('https://github.com/urbaaan/Venge-Userscript/releases/download/Graphics/graphics.js')
+			}
 		}
 	})
 
@@ -58,7 +64,8 @@ let scripts = {
 	'sepia': 'https://cdn.jsdelivr.net/gh/playcanvas/engine@master/scripts/posteffects/posteffect-sepia.js',
 	'huesaturation': 'https://cdn.jsdelivr.net/gh/playcanvas/engine@master/scripts/posteffects/posteffect-huesaturation.js',
 	'edgedetect': 'https://cdn.jsdelivr.net/gh/playcanvas/engine@master/scripts/posteffects/posteffect-edgedetect.js',
-	'contrast': 'https://cdn.jsdelivr.net/gh/playcanvas/engine@master/scripts/posteffects/posteffect-brightnesscontrast.js'
+	'contrast': 'https://cdn.jsdelivr.net/gh/playcanvas/engine@master/scripts/posteffects/posteffect-brightnesscontrast.js',
+	'fxaa': 'https://cdn.jsdelivr.net/gh/playcanvas/engine@master/scripts/posteffects/posteffect-fxaa.js'
 }
 
 Object.values(scripts).forEach((script) => {
@@ -69,21 +76,21 @@ Object.values(scripts).forEach((script) => {
 
 pc.app.on('Map:Loaded', () => {
 	let camera = pc.app.getEntityFromIndex('68d4e7a4-7063-11ea-97ae-026349a27a7c'),
-		effect = [ new BloomEffect(pc.app.graphicsDevice), new SepiaEffect(pc.app.graphicsDevice), new HueSaturationEffect(pc.app.graphicsDevice), new EdgeDetectEffect(pc.app.graphicsDevice), new BrightnessContrastEffect(pc.app.graphicsDevice) ]
+		effect = [ new BloomEffect(pc.app.graphicsDevice), new SepiaEffect(pc.app.graphicsDevice), new HueSaturationEffect(pc.app.graphicsDevice), new EdgeDetectEffect(pc.app.graphicsDevice), new BrightnessContrastEffect(pc.app.graphicsDevice), new FxaaEffect(pc.app.graphicsDevice) ]
 
 	effect[0].bloomIntensity = parseInt(localStorage.getItem('BLOOM_INTENSITY')) / 100
+	console.log( parseInt(localStorage.getItem('BLOOM_INTENSITY')) / 100)
 	effect[1].amount = parseInt(localStorage.getItem('SEPIA_AMOUNT')) / 100
 	effect[2].saturation = (localStorage.getItem('MONOTONE') === 'true') ? -1 : 0
-	if (localStorage.getItem('DARK') === 'true') {
-		effect[3] = new EdgeDetectEffect(pc.app.graphicsDevice)
-		effect[3].intensity = 2
-	}
-	else {
-		effect[3] = new BloomEffect(pc.app.graphicsDevice)
-		effect[3].bloomIntensity = 0
-	}
-	effect[4].contrast = (localStorage.getItem('CONTRAST') === 'true') ? 0.3 : 0
+	effect[3] = checkIfEnabled(localStorage.getItem('DARK'), new EdgeDetectEffect(pc.app.graphicsDevice))
+	effect[5] = checkIfEnabled(localStorage.getItem('FXAA'), new FxaaEffect(pc.app.graphicsDevice))
+	effect[4].contrast = parseInt(localStorage.getItem('CONTRAST')) / 100
+
+	effect.forEach((effects) => {
+		camera.camera.postEffects.removeEffect(effects)
+	})
 	loop()
+
 	function loop() {
 		window.requestAnimationFrame(() => {
 			effect.forEach((effects) => {
@@ -94,6 +101,18 @@ pc.app.on('Map:Loaded', () => {
 			})
 			loop()
 		})
+	}
+
+	function checkIfEnabled(value, desiredEffect) {
+		let effect
+		if (value === 'true') {
+			effect = desiredEffect
+		}
+		else {
+			effect = new BloomEffect(pc.app.graphicsDevice)
+			effect.bloomIntensity = 0
+		}
+		return effect
 	}
 })
 
